9fd2410501c3b044d6a1001f5d67e6673a228e72
Madhuri Sakhare <msak1612@gmail.com>
2022-04-30 12:21:28 +0200

    Use evaluate model for combined plotting

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,16 +1,6 @@
-from pycaret.classification import plot_model
+from pycaret.classification import evaluate_model
 
 # This function analyzes the performance of a trained model with 
 # plot/estimator as ‘class_report’ - Classification Report
 def plot_report(model):
-    plot_model(model, 'class_report')
-
-# This function analyzes the performance of a trained model with 
-# plot/estimator as ‘confusion_matrix’ - Confusion Matrix
-def plot_confusion_matrix(model):
-    plot_model(model, 'confusion_matrix')
-
-# This function analyzes the performance of a trained model with 
-# plot/estimator as ‘feature_all’ - Feature Importance (All)
-def plot_feature_importance(model):
-    plot_model(model, 'feature_all')
+    evaluate_model(model)
